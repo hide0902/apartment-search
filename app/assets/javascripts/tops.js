2c697a9fe14c881e25b854ccfc21c2ff414d83dd
hide0902 <hn.tt.sf.hu@icloud.com>
2020-03-05 00:51:20 +0900

    ビュー編集

--- app/assets/javascripts/tops.js
+++ app/assets/javascripts/tops.js
@@ -1,20 +1,7 @@
 $(function() {
   
-  $('.main-top__box__url-box').hide().slideDown(2100).animate(
+  $('.main-top__box__url-box').hide().slideDown(2200).animate(
     { opacity: 1 },
     { queue: false, duration: 500 }
   )
-  // $('.main-top__box__url-box__1').hover(function(){
-  //   $('.main-top__box__url-box__1').css('background', 'rgba(96, 99, 104, 0.8');},
-  //   function() {
-  //     $(this).css('background', '');
-  //   });
-  // $('.main-top__box__url-box__2').hover(function(){
-  //   $('.main-top__box__url-box__2').css('background', 'rgba(96, 99, 104, 0.8');},
-  //   function() {
-  //     $(this).css('background', '');
-  //   });
-  
-   
-  // $('#title').hide().fadeIn(2000);
 });
