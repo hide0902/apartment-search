8e8a195abd59e92f03974edcdd721a5b5612aeaf
hide0902 <hn.tt.sf.hu@icloud.com>
2020-03-04 13:41:53 +0900

    scc修正

--- app/assets/javascripts/tops.js
+++ app/assets/javascripts/tops.js
@@ -1,3 +1,20 @@
 $(function() {
-  $('#title').hide().fadeIn(2000);
+  
+  $('.main-top__box__url-box').hide().slideDown(2000).animate(
+    { opacity: 1 },
+    { queue: false, duration: 500 }
+  )
+  // $('.main-top__box__url-box__1').hover(function(){
+  //   $('.main-top__box__url-box__1').css('background', 'rgba(96, 99, 104, 0.8');},
+  //   function() {
+  //     $(this).css('background', '');
+  //   });
+  // $('.main-top__box__url-box__2').hover(function(){
+  //   $('.main-top__box__url-box__2').css('background', 'rgba(96, 99, 104, 0.8');},
+  //   function() {
+  //     $(this).css('background', '');
+  //   });
+  
+   
+  // $('#title').hide().fadeIn(2000);
 });
